4f8e9d9d2ffde68c27a0dc07355d4a0373e77b5a
nicoFdez <nicolafe@ucm.es>
2021-04-30 16:31:57 +0200

    Ej1 casi terminado

--- gai.cpp
+++ gai.cpp
@@ -2,14 +2,39 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
 
 int main(int argc, char **argv){
     
-    addrinfo hints;
-    addrinfo result;
+    struct addrinfo hints;
+    struct addrinfo* result;
 
-    if (argc != 2)
-        std::cout << "Invalid arguments\n";
+    if (argc != 2){
+        std::cerr << "Invalid arguments\n";
+        return -1;
+    }
 
-    int rc = (argv[1], NULL, &hints, &result);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int e = getaddrinfo(argv[1], NULL, &hints, &result);
+
+    if(e != 0){
+        std::cerr << gai_strerror(e) << "\n";
+        return -1;
+    }
+
+    struct addrinfo* it;
+    for(it = result; it !=NULL; it = it->ai_next){
+        char host[NI_MAXHOST];
+        char serv[NI_MAXHOST];
+
+        getnameinfo( it->ai_addr, it->ai_addrlen, host, NI_MAXHOST, serv, NI_MAXHOST,
+            NI_NUMERICHOST | NI_NUMERICSERV);
+
+        std::cout << "Host: "  << host << "\n";
+    }
+
+    freeaddrinfo(result);
 }
